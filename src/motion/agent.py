54daf1231406d3b41737040cb7e9e0d9ba94643a
kgw-1021 <dagonke@gmail.com>
2026-03-06 12:54:12 +0900

    Revert "update"
    This reverts commit 0e161537526814c8ebffbcb6da7d5ce4d7e392fb.

--- src/motion/agent.py
+++ src/motion/agent.py
@@ -116,7 +116,7 @@ class SampleAgent:
                 dims=[2], 
                 target_vars=other_agent.vars, 
                 time_step=t,
-                min_dist=0.8,
+                min_dist=1.0,
                 strength=20.0
             )
             
--- src/test_mulit_obs.py
+++ src/test_mulit_obs.py
@@ -7,7 +7,7 @@ from motion.obstacle import ObstacleMap
 from motion.agent import SampleAgent
 
 MAX_SIM_TIME = 1000
-COMM_RANGE = 10.0 
+COMM_RANGE = 5.0 
 
 def run_simulation():
     graph = Graph()
@@ -48,7 +48,7 @@ def run_simulation():
     fig, ax = plt.subplots(figsize=(10, 10))
     writer = PillowWriter(fps=10)
 
-    with writer.saving(fig, "simulation_result_multi_obs3.gif", 100):
+    with writer.saving(fig, "simulation_result_multi_obs2.gif", 100):
         for t in range(MAX_SIM_TIME):
             # -----------------------------------------------------
             # (A) Dynamic Topology (환경/센서 역할)
@@ -115,7 +115,7 @@ def run_simulation():
                 ax.plot(path[:, 0], path[:, 1], linestyle='-', color=c, alpha=0.6)
 
                 past = np.array(histories[i])
-                ax.plot(past[:, 0], past[:, 1], linestyle='--', linewidth=2, color=c, alpha=1.0)
+                ax.plot(past[:, 0], past[:, 1], linestyle='--', color=c, alpha=0.4)
                 
                 for neighbor_id in agent.collision_factors:
                     if neighbor_id > agent.id:
